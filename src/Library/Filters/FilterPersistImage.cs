d731679626dbb94267520a58e101af2c86be6fde
panchei19 <110833799+panchei19@users.noreply.github.com>
2022-10-24 11:45:31 -0300

    1

---
+++ src/Library/Filters/FilterPersistImage.cs
@@ -0,0 +1,13 @@
+using System.Drawing;
+using System;
+using CompAndDel;
+
+namespace CompAndDel.Filters
+{
+    public class FilterPersistImage
+    {
+/*        PictureProvider p = new PictureProvider();
+        IPicture picture = p.GetPicture(@"beer.jpg");
+        p.SavePicture(picture, @"..\..\Program\nuevaimagen.jpg");*/
+    }
+}
