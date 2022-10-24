2d76579e8e900ad827e2e13291389a65d3ed325e
panchei19 <110833799+panchei19@users.noreply.github.com>
2022-10-24 10:31:20 -0300

    parte 1 / parte 2 a medias

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,24 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+
+            static void ParteI()
+            {
+                PictureProvider provider = new PictureProvider();
+                IPicture picture = provider.GetPicture(@"beer.jpg");
+                PipeNull pipeNull = new PipeNull();
+                FilterNegative filterNegative = new FilterNegative();
+                PipeSerial pipeSerial = new PipeSerial(filterNegative, pipeNull);
+                FilterGreyscale filterGreyscale = new FilterGreyscale();
+                PipeSerial pipeSerial1 = new PipeSerial(filterGreyscale, pipeNull);
+            }
+
+            static void ParteII()
+            {
+                PictureProvider p = new PictureProvider();
+                IPicture picture = p.GetPicture(@"beer.jpg");
+IPicture        pic = p.SavePicture(picture, @"beer.jpg");
+            }
         }
     }
 }
